006cf5b3ff6de4d0247a6b715682d869d0ebcbee
Peter Hamilton <peter@inspiredpixel.net>
2011-04-28 19:06:12 +0100

    Improved delete function to handle > 1 level of inheritance. Was throwing errors for say MP3 < Audio < Media. Now works correctly

--- lib/citiesforrails/instance_methods.rb
+++ lib/citiesforrails/instance_methods.rb
@@ -1,18 +1,19 @@
 module InstanceMethods
 
   # Delete the model (and all parents it inherits from if applicable)
-  def delete
+  def delete(id = self.id)
     cities_debug("Deleting #{self.class.to_s} with ID #{self.id}")
     
     # Delete information stored in the table associated to the class of the object
     # (if there is such a table)
     deleted = true
     if(self.class.superclass!=ActiveRecord::Base)
-      # Delete up the hierarchy
-      deleted &= self.class::Clone.delete(self.id)
-      deleted &= self.class.superclass.delete(self.id)
+      cities_debug("Deleting self")
+      deleted &= self.class::Clone.delete(id)
+      cities_debug("Deleting back up hierarchy")
+      deleted &= self.class.superclass::Clone.delete(id)
     else
-      deleted &= self.class.delete(self.id)
+      deleted &= self.class.delete(id)
     end
 
     return deleted
@@ -21,7 +22,7 @@ module InstanceMethods
   def updatetype        
     sql = "UPDATE #{self.class.mother_class.table_name} SET #{self.class.inheritance_column} = '#{self.class.to_s}' WHERE id = #{self.id}"
     self.connection.execute(sql)
-    puts"#{sql}"      
+    cities_debug("#{sql}")
   end
 
   def destroy
