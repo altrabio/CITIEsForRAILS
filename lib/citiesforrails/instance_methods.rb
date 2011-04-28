e280c2aa31f75e5ef821b05bc2e63b3fcaa7f07d
Peter Hamilton <peter@inspiredpixel.net>
2011-04-28 18:09:36 +0100

    Updated delete function to correctly remove mother objects directly, was throwing an error as mother models do not have Clone available

--- lib/citiesforrails/instance_methods.rb
+++ lib/citiesforrails/instance_methods.rb
@@ -3,13 +3,16 @@ module InstanceMethods
   # Delete the model (and all parents it inherits from if applicable)
   def delete
     cities_debug("Deleting #{self.class.to_s} with ID #{self.id}")
+    
     # Delete information stored in the table associated to the class of the object
     # (if there is such a table)
     deleted = true
-    deleted &= self.class::Clone.delete(self.id)
     if(self.class.superclass!=ActiveRecord::Base)
       # Delete up the hierarchy
+      deleted &= self.class::Clone.delete(self.id)
       deleted &= self.class.superclass.delete(self.id)
+    else
+      deleted &= self.class.delete(self.id)
     end
 
     return deleted
