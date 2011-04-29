7fe467f7bd6b0becdf885a8e30317f1950623bab
Peter Hamilton <peter@inspiredpixel.net>
2011-04-29 14:28:14 +0100

    Cleaned out unnecessary comments form core_ext create_citier_view and drop_citier_view methods

--- lib/citier/core_ext.rb
+++ lib/citier/core_ext.rb
@@ -30,43 +30,20 @@ class ActiveRecord::Base
 end
 
 def create_citier_view(theclass)  #function for creating views for migrations 
-
-  if RAILS_ENV == 'development'
-    puts "CreateTheView 1"
-  end
-  self_columns = theclass::Writable.column_names.select{ |c| c != "id" } 
-  if RAILS_ENV == 'development' 
-    puts "CreateTheView 2"
-  end
+  self_columns = theclass::Writable.column_names.select{ |c| c != "id" }
   parent_columns = theclass.superclass.column_names.select{ |c| c != "id" }
   columns = parent_columns+self_columns
-  if RAILS_ENV == 'development' 
-    puts "CreateTheView 3"
-  end
   self_read_table = theclass.table_name
-  # eventuellement warning si pas de part_of
   self_write_table = theclass::Writable.table_name
   parent_read_table = theclass.superclass.table_name
-  if RAILS_ENV == 'development' 
-    puts "CreateTheView 4"
-  end
-
-  if RAILS_ENV == 'development' 
-    puts " self read table #{self_read_table} | parent_read_table #{parent_read_table}"
-  end
   sql = "CREATE VIEW #{self_read_table} AS SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id" 
+  citier_debug("Creating citier view -> #{sql}")
   theclass.connection.execute sql
 end
 
 def drop_citier_view(theclass) #function for dropping views for migrations 
-  if RAILS_ENV == 'development'
-    puts "1 DropTheViewForCITIEs"
-  end
   self_read_table = theclass.table_name
-
-  if RAILS_ENV == 'development' 
-    puts " DROP VIEW #{self_read_table}"
-  end
-  sql = "DROP VIEW #{self_read_table}" 
+  sql = "DROP VIEW #{self_read_table}"
+  citier_debug("Dropping citier view -> #{sql}")
   theclass.connection.execute sql
 end
